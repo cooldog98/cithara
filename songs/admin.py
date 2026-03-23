00871af016e5c425af3399155217107452407fbd
cooldog98 <patthiaon.p@ku.th>
2026-03-23 15:44:39 +0700

    Add create at

--- songs/admin.py
+++ songs/admin.py
@@ -1,13 +1,42 @@
 from django.contrib import admin
 from .models import Song, SongGenerationRequest, SongLibrary, ShareLink
+
+
+# Inline: แสดงเพลงใน Library
 class SongInline(admin.TabularInline):
     model = Song
     extra = 0
 
+
+# Library Admin
 class SongLibraryAdmin(admin.ModelAdmin):
     inlines = [SongInline]
 
+
+# Song Admin
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'formatted_date')
+    ordering = ('-created_at',)
+
+    def formatted_date(self, obj):
+        return obj.created_at.strftime("%d/%m/%Y %H:%M")
+
+    formatted_date.short_description = 'Created At'
+
+
+# Request Admin
+class SongGenerationRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'formatted_date')
+    ordering = ('-created_at',) #sort song new create อยุ่บนสุด
+
+    def formatted_date(self, obj):
+        return obj.created_at.strftime("%d/%m/%Y %H:%M")
+
+    formatted_date.short_description = 'Created At'
+
+
+# Register Models
 admin.site.register(SongLibrary, SongLibraryAdmin)
-admin.site.register(Song)
-admin.site.register(SongGenerationRequest)
+admin.site.register(Song, SongAdmin)
+admin.site.register(SongGenerationRequest, SongGenerationRequestAdmin)
 admin.site.register(ShareLink)
--- songs/models.py
+++ songs/models.py
@@ -30,7 +30,7 @@ class Song(models.Model):
     title = models.CharField(max_length=255)
     duration = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.title
 
