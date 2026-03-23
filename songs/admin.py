3ae67250c973434856f090955ad26d2202ff217e
cooldog98 <patthiaon.p@ku.th>
2026-03-23 15:27:01 +0700

    fix library

--- songs/admin.py
+++ songs/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Song, SongGenerationRequest, SongLibrary, ShareLink
+class SongInline(admin.TabularInline):
+    model = Song
+    extra = 0
 
+class SongLibraryAdmin(admin.ModelAdmin):
+    inlines = [SongInline]
+
+admin.site.register(SongLibrary, SongLibraryAdmin)
 admin.site.register(Song)
 admin.site.register(SongGenerationRequest)
-admin.site.register(SongLibrary)
 admin.site.register(ShareLink)
