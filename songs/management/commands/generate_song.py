5a9e4842021e9234a88363499c32d35a4b7e6822
cooldog98 <patthiaon.p@ku.th>
2026-04-20 14:08:43 +0700

    User can input the prompr to gen songs

--- songs/management/commands/generate_song.py
+++ songs/management/commands/generate_song.py
@@ -2,25 +2,34 @@ from django.core.management.base import BaseCommand
 from songs.generation import get_generator, GenerationRequest
 
 class Command(BaseCommand):
-    help = 'Demo song generation'
+    help = 'Generate a song'
 
     def handle(self, *args, **kwargs):
         generator = get_generator()
-        self.stdout.write(f"Using strategy: {generator.__class__.__name__}")
+        self.stdout.write(f"Using strategy: {generator.__class__.__name__}\n")
+
+        #get input from user
+        title = input("Song title: ")
+        prompt = input("Prompt (lyrics idea): ")
+
+        print("Occasion: birthday, wedding, anniversary, graduation christmas, party, farewell")
+        occasion = input("occasion: ")
+
+        print("Mood: happy, sad, romantic, excited, calm, nostalgic, inspiraional")
+        mood = input("mood: ")
+
+        print("Singer Gender: male, female, both")
+        singer_gender = input("Singer Gender: ")
 
         req = GenerationRequest(
-            title="My Test Song",
-            prompt="A happy birthday song for a friend",
-            mood="happy",
-            occasion="birthday",
-            singer_gender="female",
+            title=title,
+            prompt=prompt,
+            mood=mood,
+            occasion=occasion,
+            singer_gender=singer_gender,
         )
 
         result = generator.generate(req)
         self.stdout.write(f"Task ID : {result.task_id}")
-        self.stdout.write(f"Status  : {result.status}")
-        self.stdout.write(f"Audio   : {result.audio_url}")
-
-        if result.task_id:
-            status = generator.get_status(result.task_id)
-            self.stdout.write(f"Polled status: {status.status}")
+        self.stdout.write(f"Status : {result.status}")
+        self.stdout.write(f"Auto : {result.audio_url}")
